4f6c797c484c10056b02fc8b163bc73e73ec0473
Jan Librowski <janlibrowski@gmail.com>
2019-02-13 23:38:26 +0200

    initial setup

--- src/index.js
+++ src/index.js
@@ -1,32 +1,27 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import ReactDOM from 'react-dom';
-import { AppContainer } from 'react-hot-loader';
+import {AppContainer} from 'react-hot-loader';
 import Redbox from 'redbox-react';
 
 import Presentation from './presentation';
 
-const CustomErrorReporter = ({ error }) => <Redbox error={error} />;
-
-CustomErrorReporter.propTypes = {
-  error: PropTypes.instanceOf(Error).isRequired,
-};
+const CustomErrorReporter = ({error}) => <Redbox error={error}/>;
 
 ReactDOM.render(
-  <AppContainer errorReporter={CustomErrorReporter}>
-    <Presentation />
-  </AppContainer>,
-  document.getElementById('root')
+    <AppContainer errorReporter={CustomErrorReporter}>
+        <Presentation/>
+    </AppContainer>,
+    document.getElementById('root')
 );
 
 if (module.hot) {
-  module.hot.accept('./presentation', () => {
-    const NextPresentation = require('./presentation').default;
-    ReactDOM.render(
-      <AppContainer errorReporter={CustomErrorReporter}>
-        <NextPresentation />
-      </AppContainer>,
-      document.getElementById('root')
-    );
-  });
+    module.hot.accept('./presentation', () => {
+        const NextPresentation = require('./presentation').default;
+        ReactDOM.render(
+            <AppContainer errorReporter={CustomErrorReporter}>
+                <NextPresentation/>
+            </AppContainer>,
+            document.getElementById('root')
+        );
+    });
 }
---
+++ src/theme.js
@@ -0,0 +1,15 @@
+// Import theme
+import createTheme from 'spectacle/lib/themes/default';
+
+export const theme = createTheme(
+    {
+        primary: 'white',
+        secondary: '#1F2022',
+        tertiary: '#03A9FC',
+        quaternary: '#CECECE',
+    },
+    {
+        primary: 'Montserrat',
+        secondary: 'Helvetica',
+    }
+);
