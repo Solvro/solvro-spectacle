926169fced5abc9826731161d697dabda7401870
Marcin Szuster <szust.marcin@gmail.com>
2019-09-26 13:43:04 +0200

    Added the technologies logos slide

--- src/slides.js
+++ src/slides.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import 'katex/dist/katex.min.css';
-import { InlineMath, BlockMath } from 'react-katex';
-import { LineChart, Line, CartesianGrid, XAxis, Tooltip, YAxis } from 'recharts';
+import {InlineMath, BlockMath} from 'react-katex';
+import {LineChart, Line, CartesianGrid, XAxis, Tooltip, YAxis} from 'recharts';
 import CodeSlide from 'spectacle-code-slide';
 import SolvroTalksLogo from './img/logo_solvro.svg';
+import JavaScriptLogo from './img/logo_javascript.svg';
+import PythonLogo from './img/logo_python.svg';
+import AngularLogo from './img/logo_angular.svg';
+import ReactLogo from './img/logo_react.svg';
+import NodeLogo from './img/logo_node.svg';
 
 import {
     Layout,
     Fit,
+    Fill,
     Image,
     CodePane,
     Slide
@@ -33,7 +39,7 @@ export const TitleSlide = (
             margin={50}
         >
             Koło Naukowe Solvro
-      </Title>
+        </Title>
     </Slide>
 );
 
@@ -47,7 +53,7 @@ export const IframeSlide = (
             Embeddowanie nigdy nie było prostsze
         </Title>
         <Layout
-            style={{ justifyContent: 'center' }}
+            style={{justifyContent: 'center'}}
         >
             <Fit>
                 <iframe
@@ -72,7 +78,7 @@ export const YoutubeSlide = (
             Można też pokazać film
         </Title>
         <Layout
-            style={{ justifyContent: 'center' }}
+            style={{justifyContent: 'center'}}
         >
             <Fit>
                 <iframe
@@ -103,7 +109,7 @@ export const MathSlide = (
             Dla matematycznych bestii też się coś znajdzie
         </Text>
         <Layout
-            style={{ justifyContent: 'space-around' }}
+            style={{justifyContent: 'space-around'}}
         >
             <Fit>
                 <Text
@@ -127,11 +133,11 @@ export const MathSlide = (
 );
 
 const data = [
-    { name: '2015', uv: 400, pv: 2400, amt: 2400 },
-    { name: '2016', uv: 300, pv: 2400, amt: 2400 },
-    { name: '2017', uv: 213.7, pv: 2400, amt: 2400 },
-    { name: '2018', uv: 69.69, pv: 2400, amt: 2400 },
-    { name: '2019', uv: 101, pv: 2400, amt: 2400 }
+    {name: '2015', uv: 400, pv: 2400, amt: 2400},
+    {name: '2016', uv: 300, pv: 2400, amt: 2400},
+    {name: '2017', uv: 213.7, pv: 2400, amt: 2400},
+    {name: '2018', uv: 69.69, pv: 2400, amt: 2400},
+    {name: '2019', uv: 101, pv: 2400, amt: 2400}
 ];
 export const GraphSlide = (
     <Slide
@@ -143,7 +149,7 @@ export const GraphSlide = (
             Interaktywne wykresy
         </Title>
         <Layout
-            style={{ justifyContent: 'space-around' }}
+            style={{justifyContent: 'space-around'}}
         >
             <Fit>
                 <Text
@@ -152,13 +158,13 @@ export const GraphSlide = (
                     Np. Recharts
                 </Text>
                 <LineChart
-                    style={{ fontSize: '1em' }}
-                    width={800} height={400} data={data} margin={{ top: 50, right: 50, bottom: 50, left: 50 }}>
-                    <Line type="monotone" dataKey="uv" stroke="#8884d8" />
-                    <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-                    <XAxis dataKey="name" />
-                    <YAxis />
-                    <Tooltip />
+                    style={{fontSize: '1em'}}
+                    width={800} height={400} data={data} margin={{top: 50, right: 50, bottom: 50, left: 50}}>
+                    <Line type="monotone" dataKey="uv" stroke="#8884d8"/>
+                    <CartesianGrid stroke="#ccc" strokeDasharray="5 5"/>
+                    <XAxis dataKey="name"/>
+                    <YAxis/>
+                    <Tooltip/>
                 </LineChart>
             </Fit>
         </Layout>
@@ -175,11 +181,46 @@ export const PKPSlide = (
         showLineNumbers={false}
         textSize={24}
         ranges={[
-            { loc: [0, 12], title: "PKP kodzik" },
-            { loc: [0, 4], title: "Przygotowanko" },
-            { loc: [3, 4], note: "Lubimy regexy" },
-            { loc: [5, 8], note: "Jeżeli tak" },
-            { loc: [8, 12], note: "Jeżeli nie" },
+            {loc: [0, 12], title: "PKP kodzik"},
+            {loc: [0, 4], title: "Przygotowanko"},
+            {loc: [3, 4], note: "Lubimy regexy"},
+            {loc: [5, 8], note: "Jeżeli tak"},
+            {loc: [8, 12], note: "Jeżeli nie"},
         ]}
     />
+);
+
+export const TechnologiesSlide = (
+    <Slide>
+        <Title>
+            Technologie
+        </Title>
+        <Layout
+            style={{
+                marginTop: 40,
+                justifyContent: 'center'
+            }}
+        >
+            <Image
+                src={PythonLogo}
+                width={100}
+            />
+            <Image
+                src={JavaScriptLogo}
+                width={100}
+            />
+            <Image
+                src={ReactLogo}
+                width={100}
+            />
+            <Image
+                src={AngularLogo}
+                width={100}
+            />
+            <Image
+                src={NodeLogo}
+                width={100}
+            />
+        </Layout>
+    </Slide>
 );
